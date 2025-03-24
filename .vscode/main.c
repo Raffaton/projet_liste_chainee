c4bde878a31c073cabc560d59c099ee532d1749e
Raphael <raphael.souchet.85@gmail.com>
2025-03-24 13:43:26 +0100

    dossiers

---
+++ .vscode/main.c
@@ -0,0 +1,7 @@
+#include "projet.h"
+
+int main() {
+
+
+    return 0;
+}
---
+++ .vscode/projet.h
@@ -0,0 +1,8 @@
+#ifndef PROJET_H
+#define PROJET_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif

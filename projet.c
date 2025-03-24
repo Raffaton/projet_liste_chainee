a9f6fc30bfc97d895b318504143021a4f388ea15
Raphael <raphael.souchet.85@gmail.com>
2025-03-24 14:56:24 +0100

    création fonction ajout et création utilisateur et message

--- projet.c
+++ projet.c
@@ -1,1 +1,66 @@
 #include "projet.h"
+
+Utilisateurs* creer_utilisateur(int id, char pseudo[50]) {
+    Utilisateurs* nouvel_utilisateur = malloc(sizeof(Utilisateurs));
+
+    if (nouvel_utilisateur == NULL) {
+        return NULL;
+    }
+
+    nouvel_utilisateur->id = id;
+    strcpy(nouvel_utilisateur->pseudo, pseudo);
+    nouvel_utilisateur->ami_suivant = NULL;
+    nouvel_utilisateur->premiere_publication = NULL;
+    nouvel_utilisateur->utilisateur_suivant = NULL;
+
+    return nouvel_utilisateur;
+}
+
+Publications* creer_message(char texte[100]) {
+    Publications* nouveau_message = malloc(sizeof(Publications));
+
+    if (nouveau_message == NULL) {
+        return NULL;
+    }
+
+    strcpy(nouveau_message->message, texte);
+    nouveau_message->publication_suivante = NULL;
+
+    return nouveau_message;
+}
+
+void ajouter_utilisateur(Utilisateurs* premier_utilisateur,int id, char pseudo[50]) {
+    if (premier_utilisateur == NULL) {
+        return;
+    }
+
+    Utilisateurs* nouvel_utilisateur = creer_utilisateur(id, pseudo);
+    if (nouvel_utilisateur == NULL) {
+        return;
+    }
+
+    Utilisateurs* courant = premier_utilisateur;
+    while (courant->utilisateur_suivant != NULL) {
+        courant = courant->utilisateur_suivant;
+    }
+
+    courant->utilisateur_suivant = nouvel_utilisateur;
+}
+
+void ajouter_message(Publications* premier_message, char texte[100]) {
+    if (premier_message == NULL) {
+        return;
+    }
+
+    Publications* nouveau_message = creer_message(texte);
+    if (nouveau_message == NULL) {
+        return;
+    }
+
+    Publications* courant = premier_message;
+    while (courant->publication_suivante != NULL) {
+        courant = courant->publication_suivante;
+    }
+
+    courant->publication_suivante = nouveau_message;
+}
--- projet.h
+++ projet.h
@@ -7,14 +7,21 @@
 
 typedef struct {
     char message[100];
+    struct Publications* publication_suivante; 
 } Publications;
 
 typedef struct {
     int id;
     char pseudo[50];
-    struct Publications* publication_suivante;
+    struct Publications* premiere_publication;
     struct Utilisateurs* ami_suivant;
     struct Utilisateurs* utilisateur_suivant;
 } Utilisateurs;
+
+Utilisateurs* creer_utilisateur(int id, char pseudo[50]);
+Publications* creer_message(char texte[100]);
+void ajouter_utilisateur(Utilisateurs* premier_utilisateur,int id, char pseudo[50]);
+void ajouter_message(Publications* premier_message, char texte[100]);
+void afficher_utilisateurs(Utilisateurs* utilisateurs);
 
 #endif

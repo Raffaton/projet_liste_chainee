354c22fc95ad319989048d37d5650d8dcbd47ea4
Raphael <raphael.souchet.85@gmail.com>
2025-03-24 13:52:40 +0100

    création des strutcs

--- projet.h
+++ projet.h
@@ -5,4 +5,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct {
+    char message[100];
+} Publications;
+
+typedef struct {
+    int id;
+    char pseudo[50];
+    struct Publications* publication_suivante;
+    struct Utilisateurs* ami_suivant;
+    struct Utilisateurs* utilisateur_suivant;
+} Utilisateurs;
+
 #endif

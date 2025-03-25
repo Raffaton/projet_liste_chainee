f1b8b4af32bf1779f1d24c8e8520b10e41ef62bb
Arthur PAILLE <a.paille@lyceesaintefamille.com>
2025-03-25 09:27:52 +0100

    debut

--- projet.c
+++ projet.c
@@ -44,7 +44,6 @@ void ajouter_utilisateur(Utilisateurs* premier_utilisateur, int id, char pseudo[
         courant = courant->utilisateur_suivant;
     }
 
-
     courant->utilisateur_suivant = nouvel_utilisateur;
 }
 
@@ -65,19 +64,65 @@ void ajouter_message(Publications* premier_message, char texte[100]) {
 
     courant->publication_suivante = nouveau_message;
 }
-void afficher_menu() {
-    printf("\n--- Réseau Social ---\n");
-    printf("1. Ajouter un utilisateur\n");
-    printf("2. Afficher tous les utilisateurs\n");
-    printf("3. Quitter\n");
+void ajouter_ami(Utilisateurs* utilisateurs, int id_utilisateur, int id_ami) {
+	Utilisateurs* utilisateur = trouver_utilisateur(utilisateurs, id_utilisateur);
+	Utilisateurs* ami = trouver_utilisateur(utilisateurs, id_ami);
+
+	if (!utilisateur || !ami) {
+		printf("Utilisateur ou ami introuvable.\n");
+		return;
+	}
+
+=	Utilisateurs* courant = utilisateur->ami_suivant;
+	while (courant != NULL) {
+		if (courant->id == id_ami) {
+			printf("Cet utilisateur est déjà un ami.\n");
+			return;
+		}
+		courant = courant->ami_suivant;
+	}
+
+	Utilisateurs* nouvel_ami = malloc(sizeof(Utilisateurs));
+	if (!nouvel_ami) {
+		printf("Erreur mémoire.\n");
+		return;
+	}
+	nouvel_ami->id = ami->id;
+	strcpy(nouvel_ami->pseudo, ami->pseudo);
+	nouvel_ami->premiere_publication = NULL;
+	nouvel_ami->ami_suivant = utilisateur->ami_suivant;
+	nouvel_ami->utilisateur_suivant = NULL;
+	utilisateur->ami_suivant = nouvel_ami;
+
+	printf("Ami ajouté avec succès.\n");
+
+
+void ajouter_publication(Utilisateurs* utilisateurs, int id) {
+    if (utilisateurs == NULL) {
+        return;
+    }
+
+    Utilisateurs* courant = utilisateurs;
+    while (courant->id != id) {
+        courant = courant->utilisateur_suivant;
+    }
+
+    char texte[100];
+    printf("Entrez votre publication (max 100 char) :\n");
+    fgets(texte, 100, stdin);
+
+    if (courant->premiere_publication == NULL) {
+        courant->premiere_publication = creer_message(texte);
+    }
+    else {
+        ajouter_message(courant->premiere_publication, texte);
+    }
 }
 
 void afficher_utilisateurs(Utilisateurs* utilisateurs) {
     if (utilisateurs == NULL) {
-        printf("Aucun utilisateur enregistré.\n");
         return;
     }
-
 
     Utilisateurs* courant = utilisateurs;
     printf("\nListe des utilisateurs :\n");
@@ -85,5 +130,34 @@ void afficher_utilisateurs(Utilisateurs* utilisateurs) {
         printf("ID: %d, Pseudo: %s\n", courant->id, courant->pseudo);
         courant = courant->utilisateur_suivant;
     }
-    
+}
+
+void afficher_info(Utilisateurs* utilisateurs, int id) {
+    if (utilisateurs == NULL) {
+        return;
+    }
+
+    Utilisateurs* courant = utilisateurs;
+    while (courant->id != id) {
+        courant = courant->utilisateur_suivant;
+    }
+
+    printf("Amis de %s :\n", courant->pseudo);
+    while (courant->ami_suivant != NULL) {
+        Utilisateurs* actuel = courant->ami_suivant;
+        printf(" - ID: %d, Pseudo: %s\n", actuel->id, actuel->pseudo);
+        
+    }
+
+}
+
+
+void afficher_menu() {
+    printf("\n=== Reseau Social ===\n");
+    printf("1. Ajouter un utilisateur\n");
+    printf("2. Ajouter un ami\n");
+    printf("3. Ajouter une publication\n");
+    printf("4. Afficher tous les utilisateurs\n");
+    printf("5. Afficher les informations d'un utilisateur\n");
+    printf("6. Quitter\n");
 }
